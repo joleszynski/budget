ed412c220e078d92833bc4e868b859d122c3e0de
Jan <jan.oleszynski.it@gmail.com>
2022-04-09 13:16:38 +0200

    Add comma to INSTALLED_APPS=[...main,]

--- budgetapp/settings.py
+++ budgetapp/settings.py
@@ -39,8 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'main',
     ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
